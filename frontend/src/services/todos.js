5b800b361c5238e53100121695f343039e60172b
RuMarS <82797859+RuMarS@users.noreply.github.com>
2022-11-15 14:32:24 +0400

    Update todos.js

--- frontend/src/services/todos.js
+++ frontend/src/services/todos.js
@@ -4,37 +4,37 @@ class TodoDataService {
     getAll(token) {
         axios.defaults.headers.common["Authorization"] = `Token ${token}` ;//"Token"+ token;
         //alert( "axios "+ axios.defaults.headers.common["Authorization"]);
-        return axios.get("http://user24122010.pythonanywhere.com/api/todos/");
+        return axios.get("http://localhost:8000/api/todos/");
     }
 
     createTodo(data, token) {
         axios.defaults.headers.common["Authorization"] = `Token ${token}`;
        // alert("axios createTodo");
-        return axios.post("http://user24122010.pythonanywhere.com/api/todos/", data);
+        return axios.post("http://localhost:8000/api/todos/", data);
     }
 
     updateTodo(id, data, token) {
         axios.defaults.headers.common["Authorization"] = `Token ${token}`;
       //  alert("axios updateTodo");
-        return axios.put("http://user24122010.pythonanywhere.com/api/todos/"+`${id}`, data);
+        return axios.put("http://localhost:8000/api/todos/"+`${id}`, data);
     }
 
     deleteTodo (id, token) {
         axios.defaults.headers.common["Authorization"] = `Token ${token}`;
-        return axios.delete("http://user24122010.pythonanywhere.com/api/todos/"+`${id}`);
+        return axios.delete("http://localhost:8000/api/todos/"+`${id}`);
     }
 
     completeTodo(id, token){
         axios.defaults.headers.common["Authorization"] = `Token ${token}`;
-        return axios.put("http://user24122010.pythonanywhere.com/api/todos/"+`${id}`+"/complete");
+        return axios.put("http://localhost:8000/api/todos/"+`${id}`+"/complete");
     }
 
     login(data){
-        return axios.post("http://user24122010.pythonanywhere.com/api/login/", data);
+        return axios.post("http://localhost:8000/api/login/", data);
     }
 
     signup(data){
-        return axios.post("http://user24122010.pythonanywhere.com/api/signup/", data);
+        return axios.post("http://localhost:8000/api/signup/", data);
     }
 
 }
